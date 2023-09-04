f38dddc040ab39eced7c51a38b4cf7c24af992d1
Shakeel Akthar <shakeelakhther@gmail.com>
2023-09-05 00:24:48 +0530

    static

--- todo/settings.py
+++ todo/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+# }
 
 
 #external db
